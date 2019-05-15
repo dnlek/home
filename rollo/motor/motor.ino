877ceb2b9d288424b09d96b2c75501ad4dbbeda0
Daniel Wagner <dnl.wagner@gmail.com>
2019-05-15 22:05:52 +0200

    :sparkles: Improved deep sleep, added serial handler, added button interrupt

--- rollo/motor/motor.ino
+++ rollo/motor/motor.ino
@@ -7,6 +7,7 @@
 #include <avr/sleep.h>
 #include <avr/power.h>
 #include <avr/wdt.h>
+#include <LowPower.h>
 
 #define DEFAULT_SPEED "2" // 1 or 2
 
@@ -17,34 +18,41 @@ int buttonPin = 2;
 RF24 radio(7, 8); //CE, CSN
 const byte address[6] = "00001";
 Servo servo;
+char receivedChar;
+bool runServo = false;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("SETUP RECEIVER");
+  
   radio.begin();
-  Serial.println(radio.isChipConnected());
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
 
   pinMode(buttonPin, INPUT_PULLUP);
-
+  
   // Avoid Randomly Vibrating when turning on
-//  servo.attach(servoPin);
-//  servo.write(90); 
-//  delay(200); 
-//  servo.detach();
+  //XXX not helping too much
+  //  servo.attach(servoPin);
+  //  servo.write(90); 
+  //  delay(200); 
+  //  servo.detach();
 
-  MCUSR &= ~(1 <<WDRF);
-
-  WDTCSR |= (1<<WDCE) | (1<<WDE);
-
-  WDTCSR = 1<<WDP0 | 1<<WDP3; // 8.0 seconds
-
-  WDTCSR |= _BV(WDIE);
+  // watchdog config for 8s sleep
+  //  MCUSR &= ~(1 <<WDRF);
+  //  WDTCSR |= (1<<WDCE) | (1<<WDE);
+  //  WDTCSR = 1<<WDP0 | 1<<WDP3; // 8.0 seconds
+  //  WDTCSR |= _BV(WDIE);
 
   Serial.println("Initialization complete..");
   delay(100);
+}
+
+void wakeUp() {
+    // Just a handler for the pin interrupt.
+    Serial.println("Brutal wake up");
+    runServo = true;
 }
 
 void loop() {
@@ -52,12 +60,19 @@ void loop() {
   // Serial.println("Battery level: " + getBatteryLevel());
   // delay(500);
   // Serial.println("Vcc: " + readVcc());
+  attachInterrupt(digitalPinToInterrupt(buttonPin), wakeUp, FALLING);
+  
+  Serial.println("Enter sleep");
   delay(500);
-  enterSleep();
+  // enterSleep();
+  LowPower.powerDown(SLEEP_4S, ADC_OFF, BOD_OFF);  
   delay(500);
+  Serial.println("Wake up");
+
+  detachInterrupt(digitalPinToInterrupt(buttonPin));
   
   if (radio.available()) {
-    Serial.println("Have message");
+    Serial.println("Message - on the radio");
     char text[32] = "";
     radio.read(&text, sizeof(text));
     Serial.println(text);
@@ -69,12 +84,24 @@ void loop() {
     }
   }
 
-  if (digitalRead(buttonPin) == LOW) {
-    Serial.println("Button pressed");
+  if (digitalRead(buttonPin) == LOW || runServo) {
     openOrClose(0);
+    runServo = false;
   }
   
   delay(100);
+}
+
+void serialEvent() {
+  if (Serial.available() > 0) {
+    Serial.println("Message - on the serial");
+    receivedChar = Serial.read();
+    if (receivedChar == 'o') {
+      openOrClose(1);
+    } else if (receivedChar == 'c') {
+      openOrClose(0);
+    }
+  }
 }
 
 void openOrClose(int direction) {
@@ -126,11 +153,11 @@ int getBatteryLevel() {
   return result;
 }
 
-void enterSleep() {
-  set_sleep_mode(SLEEP_MODE_PWR_SAVE);
-  sleep_enable();
-  
-  sleep_disable();
-  power_all_enable();
-  
-}
+//void enterSleep() {
+//  set_sleep_mode(SLEEP_MODE_PWR_SAVE);
+//  sleep_enable();
+//  
+//  sleep_disable();
+//  power_all_enable();
+//  
+//}
